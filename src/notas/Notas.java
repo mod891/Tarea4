ee24477d5cc466732154a9f99656c4f17d748324
david motos olmedo <skuayer91@gmail.com>
2022-05-27 16:07:59 +0200

    Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás cali, que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse y en Netbeans). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class Notas {
 
 public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-int n = 0;
-String mensaje = "";
-System.out.print("Introduzca una nota: ");
-n = reader.nextInt();
+cali();
+}
 
-if (n >= 0 && n < 5)
-mensaje = "Suspenso";
-else if (n >= 5 && n < 7)
-mensaje = "Aprobado";
-else if (n >= 7 && n <9)
-mensaje = "Notable";
-else if (n >= 9 && n <= 10)
-mensaje = "Sobresaliente";
-else
-mensaje = "La nota introducida no es correcta";
-
-System.out.println(mensaje);
-reader.close();
+public static void cali() {
+	Scanner reader = new Scanner(System.in);// Para pedir números por teclado
+	int n = 0;
+	String mensaje = "";
+	System.out.print("Introduzca una nota: ");
+	n = reader.nextInt();
+	
+	if (n >= 0 && n < 5)
+	mensaje = "Suspenso";
+	else if (n >= 5 && n < 7)
+	mensaje = "Aprobado";
+	else if (n >= 7 && n <9)
+	mensaje = "Notable";
+	else if (n >= 9 && n <= 10)
+	mensaje = "Sobresaliente";
+	else
+	mensaje = "La nota introducida no es correcta";
+	
+	System.out.println(mensaje);
+	reader.close();
 }
 }
